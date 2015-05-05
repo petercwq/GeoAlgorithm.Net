a051693fcbca6554339431a0381a2eaf97d664b3
Peter Chen <peter_chen@trimble.com>
2015-05-05 18:51:04 +0800

    aaa

--- GeoAlgorithm.Net/GeoAlgorithm.Net/QuadTree.cs
+++ GeoAlgorithm.Net/GeoAlgorithm.Net/QuadTree.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace GeoAlgorithm.Net
 {
+    /// <summary>
+    /// Reference http://architects.dzone.com/articles/algorithm-week-spatial
+    /// </summary>
     class QuadTree
     {
     }
