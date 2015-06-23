9ff0a046f173ebe7168596de3193afd6cc214206
Peter Chen <peter_chen@trimble.com>
2015-06-23 21:41:41 +0800

    Add caches

---
+++ GeoAlgorithm.Net/Cache/LRUCacheHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace Trimble.Ems.Cross.Types
+{
+    public class LRUCacheHelper<K, V>
+    {
+        readonly Dictionary<K, V> _dict;
+        readonly LinkedList<K> _queue = new LinkedList<K>();
+        readonly object _syncRoot = new object();
+        private readonly int _max;
+        public LRUCacheHelper(int capacity)
+        {
+            _dict = new Dictionary<K, V>(capacity);
+            _max = capacity;
+        }
+
+        public void Add(K key, V value)
+        {
+            lock (_syncRoot)
+            {
+                CheckCapacity();
+                _queue.AddLast(key);                            //O(1)
+                _dict[key] = value;                             //O(1)
+            }
+        }
+
+        private void CheckCapacity()
+        {
+            lock (_syncRoot)
+            {
+                int count = _dict.Count;                        //O(1)
+                if (count == _max)
+                {
+                    // cache full, so re-use the oldest node
+                    var node = _queue.First;
+                    _dict.Remove(node.Value);                   //O(1)
+                    _queue.RemoveFirst();                       //O(1)
+                }
+            }
+        }
+
+        public void Delete(K key)
+        {
+            lock (_syncRoot)
+            {
+                _dict.Remove(key);                              //O(1)
+                _queue.Remove(key);                             //O(n)
+            }
+        }
+
+        public V Get(K key)
+        {
+            lock (_syncRoot)
+            {
+                V ret;
+                if (_dict.TryGetValue(key, out ret))            //O(1)
+                {
+                    _queue.Remove(key);                         //O(n)
+                    _queue.AddLast(key);                        //O(1)
+                }
+                return ret;
+            }
+        }
+    }
+}
---
+++ GeoAlgorithm.Net/Cache/LeastRecentlyUsedCache.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Trimble.Ems.Cross.Types
+{
+    public class LeastRecentlyUsedCache<TKey, TValue> where TValue : class, IDisposable
+    {
+        private readonly ConcurrentDictionary<TKey, Node> entries;
+        private readonly int capacity;
+        private Node head;
+        private Node tail;
+
+        private class Node
+        {
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+        }
+
+        public LeastRecentlyUsedCache(int capacity = 16)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException(
+                    "capacity",
+                    "Capacity should be greater than zero");
+            this.capacity = capacity;
+            entries = new ConcurrentDictionary<TKey, Node>();
+            head = null;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            Node entry;
+            if (!entries.TryGetValue(key, out entry))
+            {
+                entry = new Node { Key = key, Value = value };
+                if (entries.Count == capacity)
+                {
+                    Node node;
+                    entries.TryRemove(tail.Key, out node);
+                    tail = tail.Previous;
+                    if (tail != null)
+                        tail.Next = null;
+                }
+                entries.TryAdd(key, entry);
+            }
+
+            entry.Value = value;
+            MoveToHead(entry);
+            if (tail == null)
+                tail = head;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            value = default(TValue);
+            Node entry;
+            if (!entries.TryGetValue(key, out entry))
+                return false;
+            MoveToHead(entry);
+            value = entry.Value;
+            return true;
+        }
+
+        public void Purge()
+        {
+            foreach (var element in entries)
+                element.Value.Value.Dispose();
+
+            entries.Clear();
+        }
+
+        private void MoveToHead(Node entry)
+        {
+            if (entry == head || entry == null) return;
+
+            var next = entry.Next;
+            var previous = entry.Previous;
+
+            if (next != null)
+                next.Previous = entry.Previous;
+            if (previous != null)
+                previous.Next = entry.Next;
+
+            entry.Previous = null;
+            entry.Next = head;
+
+            if (head != null)
+                head.Previous = entry;
+            head = entry;
+
+            if (tail == entry)
+                tail = previous;
+        }
+    }
+}
