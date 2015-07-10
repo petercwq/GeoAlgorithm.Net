f8fdfdff6cf7b71e003fdaabcfc9caa0a1f6b6c5
Peter Chen <peter_chen@trimble.com>
2015-07-10 15:43:46 +0800

    add convex_hull algorithm

---
+++ ConvexHullDemoApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ConvexHullDemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(2, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "howto_convex_hull";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ConvexHullDemoApp/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+using GeoAlgorithm.Net.ConvexHull;
+
+namespace ConvexHullDemoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Point> m_Points = new List<Point>();
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            m_Points = new List<Point>();
+            this.Invalidate();
+        }
+
+        // Add a new Point.
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_Points.Add(new Point(e.X, e.Y));
+            this.Invalidate();
+        }
+
+        // Redraw.
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Draw the convex hull.
+
+            // Fill all of the points.
+            foreach (Point pt in m_Points)
+            {
+                e.Graphics.FillEllipse(Brushes.Cyan, pt.X - 3, pt.Y - 3, 7, 7);
+            }
+
+            List<ConvexHull.Point> hull = null;
+            if (m_Points.Count >= 3)
+            {
+                // Get the convex hull.
+                hull = ConvexHull.MakeConvexHull(m_Points.Select(p => new ConvexHull.Point(p.X, p.Y)).ToList());
+
+                // Draw.
+                // Fill the non-culled points.
+                foreach (var pt in ConvexHull.g_NonCulledPoints)
+                {
+                    e.Graphics.FillEllipse(Brushes.White, (float)pt.X - 3, (float)pt.Y - 3, 7, 7);
+                }
+            }
+
+            // Draw all of the points.
+            foreach (Point pt in m_Points)
+            {
+                e.Graphics.DrawEllipse(Pens.Black, pt.X - 3, pt.Y - 3, 7, 7);
+            }
+
+            if (m_Points.Count >= 3)
+            {
+                // Draw the MinMax quadrilateral.
+                e.Graphics.DrawPolygon(Pens.Red, ConvexHull.g_MinMaxCorners.Select(p => new Point((int)p.X, (int)p.Y)).ToArray());
+
+                // Draw the culling box.
+                var box = ConvexHull.g_MinMaxBox;
+                e.Graphics.DrawRectangle(Pens.Orange, new Rectangle((int)box.Left, (int)box.Top, (int)(box.Right - box.Left), (int)(box.Bottom - box.Top)));
+
+                // Draw the convex hull.
+                var hull_points = new ConvexHull.Point[hull.Count];
+                hull.CopyTo(hull_points);
+                e.Graphics.DrawPolygon(Pens.Blue, hull_points.Select(p => new Point((int)p.X, (int)p.Y)).ToArray());
+            }
+        }
+    }
+}
